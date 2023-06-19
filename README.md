fbf56caa23f4f1ca0b3e0ba349032f07f8390ee8
Kranthisai07 <kranthisaigadi@gmail.com>
2023-06-19 12:13:50 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Kranthi's-GRIP-bank
+If you enjoy the work, fork over the money (fork the respo). 
+On a local workstation, clone the repository. 
+
+Create the "kranthidb" database or any other named database (modify the spin.php settings if you create any other named database).
+Use "kranthidb.sql" to import the tables.
---
+++ kranthidb.sql
@@ -0,0 +1,161 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 08, 2021 at 06:52 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `kranthidb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contactus`
+--
+
+CREATE TABLE `contactus` (
+  `sr` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(20) NOT NULL,
+  `message` mediumtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `contactus`
+--
+
+INSERT INTO `contactus` (`sr`, `name`, `email`, `message`) VALUES
+(1, 'Sree',	'sree79@gmail.com', 'This is demo paragraph!'),
+(3, 'Shankar',	'dshankar@gmail.com', 'This is a para!'),
+(6, 'Latha', 'lathasree@gmail.com', 'this is my new contact email'),
+(18, 'Amrutha', 'amrutha@gmaill.com', 'Awesome!'),
+(24, 'Abhishek', 'abhishek69@gmail.com', 'What is the python course fee?'),
+(26, 'Sai', 'sai90@gmail.com', 'Hello!'),
+(30, 'Sreeja', 'sreeja@gmail.com', 'joy!!');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transactions`
+--
+
+CREATE TABLE `transactions` (
+  `sr` int(11) NOT NULL,
+  `sender` varchar(10) NOT NULL,
+  `receiver` varchar(10) NOT NULL,
+  `amount` bigint(20) NOT NULL,
+  `status` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `transactions`
+--
+
+INSERT INTO `transactions` (`sr`, `sender`, `receiver`, `amount`, `status`) VALUES
+(1, '7311780855', '5353112532', 200, 'succeed'),
+(2, '', '', 0, 'failed'),
+(3, '', '', 0, 'failed'),
+(4, '', '', 0, 'failed'),
+(5, '', '', 0, 'failed'),
+(6, '7311780855', '5353112532', 100, 'succeed'),
+(7, '7311780855', '5353112532', 0, 'failed'),
+(8, '7311780855', '4001286186', 200, 'succeed'),
+(9, '7311780855', '4001286186', 500, 'succeed'),
+(10, '3917416800', '4001286186', 14000, 'failed'),
+(11, '', '', 0, 'failed'),
+(12, '7311780855', '4001286186', 9950000, 'succeed'),
+(13, '4001286186', '7311780855', 500, 'succeed'),
+(14, '4001286186', '5353112532', 800, 'succeed');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `sr` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `email` varchar(20) NOT NULL,
+  `accno` varchar(10) NOT NULL,
+  `blc` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`sr`, `name`, `email`, `accno`, `blc`) VALUES
+(1, 'sree','sree79@gmail.com', '6353110426', 31209),
+(2, 'Latha', 'lathasree@gmail.com', '8911781475', 13000),
+(3, 'Abhishek', 'abhishek69@gmail.com', '7801286463', 250000),
+(4, 'Shankar', 'dshankar@gmail.com', '6997486422', 36490),
+(5, 'Amrutha', 'amrutha@gmail.com', '2559726175', 15340),
+(6, 'Sreeja', 'sreeja@gmail.com', '9820431026', 19950),
+(7, 'Sai', 'sai90@gmail.com', '9104806969', 32490),
+(8, 'Mamatha', 'kattapa@gmail.com', '9984629420', 300),
+(9, 'sravya', 'sravya@gmail.com', '7517417798', 50100),
+(10, 'Likitha', 'likitha77@gmail.com', '7702696975', 6000);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `contactus`
+--
+ALTER TABLE `contactus`
+  ADD PRIMARY KEY (`sr`);
+
+--
+-- Indexes for table `transactions`
+--
+ALTER TABLE `transactions`
+  ADD PRIMARY KEY (`sr`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`sr`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `contactus`
+--
+ALTER TABLE `contactus`
+  MODIFY `sr` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT for table `transactions`
+--
+ALTER TABLE `transactions`
+  MODIFY `sr` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `sr` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
